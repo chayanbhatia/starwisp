6f4dcc8cf5ec84c62dd174c913ff22c2b106a157
alizahir23 <alizahiruddin@gmail.com>
2020-08-30 13:07:06 +0300

    home page built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-
+import { redirect, useHistory } from 'react-router-dom'
 import styles from './css/App.module.css';
-
+import help from './help.svg'
+import notification from './notification.svg'
 function App() {
+
+  let history = useHistory()
+  const handleClick = () => {
+    console.log(history)
+  }
   return (
     <div className="App">
-      <div className={styles['header']}></div>
+      <div className={styles['header']}>
+        <div><h1>LOGO</h1></div>
+        <div>
+          <img src={help} alt="" />
+          <img src={notification} alt="" />
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.sidebar}>
+          <p>DASHBOARD</p>
+          <p>UNIVERSITY</p>
+          <p>SYSTEM</p>
+        </div>
+        <div className={styles.main}>
+          <div onClick={handleClick} className={styles.item1}>QUOTATION</div>
+          <div className={styles.item2}>REVENUE</div>
+          <div className={styles.item3}>SALES REPORT</div>
+          <div className={styles.item4}>SERVICES SOLD</div>
+          <div className={styles.item5}>RECENT ACTIVITY</div>
+          <div className={styles.item6}>LATEST MESSAGES</div>
+          <div className={styles.item7}>SOCIAL SOURCE</div>
+        </div>
+      </div>
     </div>
   );
 }
